681ebf5e1c0487bcf9113693f2048b337f16c751
4rka6 <4rka6@users.noreply.github.com>
2026-06-07 15:53:27 +0000

    Remove local build folder

--- build/CMakeFiles/banking.dir/src/Account.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/banking.dir/src/Account.cpp.o: \
- /home/apkent/apkent/workspace/projects/lab05/src/Account.cpp \
- /usr/include/stdc-predef.h \
- /home/apkent/apkent/workspace/projects/lab05/include/Account.h

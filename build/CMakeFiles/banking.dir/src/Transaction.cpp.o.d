fa3394a349c85c3ecda5c0722ef7db25bf4c8d67
4rka6 <4rka6@users.noreply.github.com>
2026-06-09 18:34:09 +0000

    LR05: final + Mock

---
+++ build/CMakeFiles/banking.dir/src/Transaction.cpp.o.d
@@ -0,0 +1,6 @@
+CMakeFiles/banking.dir/src/Transaction.cpp.o: \
+ /home/apkent/apkent/workspace/projects/lab05/src/Transaction.cpp \
+ /usr/include/stdc-predef.h \
+ /home/apkent/apkent/workspace/projects/lab05/include/Transaction.h \
+ /home/apkent/apkent/workspace/projects/lab05/include/Account.h \
+ /home/apkent/apkent/workspace/projects/lab05/include/IValidator.h
